a53eab286da951d568fd40f0af4f47a67c74edb2
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2024-02-12 23:44:40 -0800

    Fix lavaMD

--- src/lavaMD-pyomp/main.py
+++ src/lavaMD-pyomp/main.py
@@ -1,11 +1,10 @@
 from numba import njit
 from numba.openmp import openmp_context as openmp
-from numba.openmp import omp_get_wtime, omp_set_num_threads, omp_get_num_threads, omp_get_num_devices, omp_is_initial_device, omp_get_team_num, omp_get_thread_num
+from numba.openmp import omp_get_wtime, omp_set_num_threads, omp_get_num_threads, omp_get_num_devices, omp_is_initial_device, omp_get_team_num, omp_get_thread_num, omp_get_num_teams
 import numba
 import numpy as np
 import sys
 import math
-import collections
 
 # ----------------------------------------------------------------
 
@@ -102,6 +101,20 @@ def core(dim_cpu_boxes1d_arg,
          NUMBER_PAR_PER_BOX,
          NUMBER_THREADS,
          par_cpu_alpha):
+
+  # TODO this is a workaround to allocate from global memory scaled to number
+  # of teams, those arrays should be in shared memory for optimization
+  # NOTE: 100 = NUMBER_PAR_PER_BOX
+  rA_shared_v = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rA_shared_x = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rA_shared_y = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rA_shared_z = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rB_shared_v = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rB_shared_x = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rB_shared_y = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  rB_shared_z = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+  qB_shared = np.empty((dim_cpu_number_boxes,100), dtype=np.float32)
+
   with openmp("""target data
                    map(to: box_cpu_x,
                            box_cpu_y,
@@ -119,130 +132,140 @@ def core(dim_cpu_boxes1d_arg,
                            rv_cpu_y,
                            rv_cpu_z,
                            qv_cpu)
-                   map(tofrom: fv_cpu_v,
-                               fv_cpu_x,
-                               fv_cpu_y,
-                               fv_cpu_z)"""):
-    kstart = omp_get_wtime()
-    with openmp("target teams num_teams(dim_cpu_number_boxes) thread_limit(NUMBER_THREADS)"):
-      rA_shared_v = omp_shared_array(100, np.float32)
-      rA_shared_x = omp_shared_array(100, np.float32)
-      rA_shared_y = omp_shared_array(100, np.float32)
-      rA_shared_z = omp_shared_array(100, np.float32)
-      rB_shared_v = omp_shared_array(100, np.float32)
-      rB_shared_x = omp_shared_array(100, np.float32)
-      rB_shared_y = omp_shared_array(100, np.float32)
-      rB_shared_z = omp_shared_array(100, np.float32)
-      qB_shared   = omp_shared_array(100, np.float32)
-      """
-      rA_shared_v = np.empty(100, dtype=np.float32)
-      rA_shared_x = np.empty(100, dtype=np.float32)
-      rA_shared_y = np.empty(100, dtype=np.float32)
-      rA_shared_z = np.empty(100, dtype=np.float32)
-      rB_shared_v = np.empty(100, dtype=np.float32)
-      rB_shared_x = np.empty(100, dtype=np.float32)
-      rB_shared_y = np.empty(100, dtype=np.float32)
-      rB_shared_z = np.empty(100, dtype=np.float32)
-      qB_shared = np.empty(100, dtype=np.float32)
-      """
 
-      with openmp("parallel"):
-        bx = omp_get_team_num()
-        tx = omp_get_thread_num()
-        wtx = tx
+                   map(tofrom:
+                           fv_cpu_v,
+                           fv_cpu_x,
+                           fv_cpu_y,
+                           fv_cpu_z)
 
-        #  DO FOR THE NUMBER OF BOXES
+                    map(alloc:
+                          rA_shared_v,
+                          rA_shared_x,
+                          rA_shared_y,
+                          rA_shared_z,
+                          rB_shared_v,
+                          rB_shared_x,
+                          rB_shared_y,
+                          rB_shared_z,
+                          qB_shared)
 
-        if bx < dim_cpu_number_boxes:
-          # parameters
-          a2 = 2 * par_cpu_alpha * par_cpu_alpha
+                           device(1)"""):
+      kstart = omp_get_wtime()
+      with openmp("target teams num_teams(dim_cpu_number_boxes) thread_limit(NUMBER_THREADS) device(1)"):
+        # TODO: allocate using numba cuda interface when in CUDA compilation,
+        # needs fallback for host code.
+        #rA_shared_v = numba.cuda.shared.array(100, dtype=np.float32)
+        #rA_shared_x = numba.cuda.shared.array(100, dtype=np.float32)
+        #rA_shared_y = numba.cuda.shared.array(100, dtype=np.float32)
+        #rA_shared_z = numba.cuda.shared.array(100, dtype=np.float32)
+        #rB_shared_v = numba.cuda.shared.array(100, dtype=np.float32)
+        #rB_shared_x = numba.cuda.shared.array(100, dtype=np.float32)
+        #rB_shared_y = numba.cuda.shared.array(100, dtype=np.float32)
+        #rB_shared_z = numba.cuda.shared.array(100, dtype=np.float32)
+        #qB_shared = numba.cuda.shared.array(100, dtype=np.float32)
 
-          # nei box
-          k = 0
-          j = 0
-
-          # home box - box parameters
-          first_i = box_cpu_offset[bx]
-
-          # home box - shared memory
-          while wtx < NUMBER_PAR_PER_BOX:
-            rA_shared_v[wtx] = rv_cpu_v[first_i+wtx]
-            rA_shared_x[wtx] = rv_cpu_x[first_i+wtx]
-            rA_shared_y[wtx] = rv_cpu_y[first_i+wtx]
-            rA_shared_z[wtx] = rv_cpu_z[first_i+wtx]
-            wtx += NUMBER_THREADS
+        with openmp("parallel"):
+          bx = omp_get_team_num()
+          tx = omp_get_thread_num()
           wtx = tx
 
-          with openmp("barrier"):
-              pass
+          #  DO FOR THE NUMBER OF BOXES
+          if bx < dim_cpu_number_boxes:
+            # parameters
+            a2 = 2 * par_cpu_alpha * par_cpu_alpha
 
-          # loop over nei boxes of home box
-          for k in range(1 + box_cpu_nn[bx]):
+            # nei box
+            k = 0
+            j = 0
 
-            #----------------------------------------50
-            #  nei box - get pointer to the right box
-            #----------------------------------------50
+            # home box - box parameters
+            first_i = box_cpu_offset[bx]
 
-            if k == 0:
-              # set first box to be processed to home box
-              pointer = bx
-            else:
-              # remaining boxes are nei boxes
-              pointer = box_cpu_nei_number[bx, k-1]
-
-            #  Setup parameters
-
-            # nei box - box parameters
-            first_j = box_cpu_offset[pointer]
-
-            # (enable the section below only if wanting to use shared memory)
-            # nei box - shared memory
+            # home box - shared memory
             while wtx < NUMBER_PAR_PER_BOX:
-              rB_shared_v[wtx] = rv_cpu_v[first_j+wtx]
-              rB_shared_x[wtx] = rv_cpu_x[first_j+wtx]
-              rB_shared_y[wtx] = rv_cpu_y[first_j+wtx]
-              rB_shared_z[wtx] = rv_cpu_z[first_j+wtx]
-              qB_shared[wtx] = qv_cpu[first_j+wtx]
+              rA_shared_v[bx,wtx] = rv_cpu_v[first_i+wtx]
+              rA_shared_x[bx,wtx] = rv_cpu_x[first_i+wtx]
+              rA_shared_y[bx,wtx] = rv_cpu_y[first_i+wtx]
+              rA_shared_z[bx,wtx] = rv_cpu_z[first_i+wtx]
               wtx += NUMBER_THREADS
             wtx = tx
 
-            # (enable the section below only if wanting to use shared memory)
-            # synchronize threads because in next section each thread accesses data brought in by different threads here
             with openmp("barrier"):
                 pass
+            # loop over nei boxes of home box
+            #if tx == 0:
+            #  print("line 189", "bx", bx, "tx", tx, "range", box_cpu_nn[bx])
+            for k in range(1 + box_cpu_nn[bx]):
 
-            #  Calculation
+              #----------------------------------------50
+              #  nei box - get pointer to the right box
+              #----------------------------------------50
 
-            # loop for the number of particles in the home box
-            while wtx < NUMBER_PAR_PER_BOX:
+              if k == 0:
+                # set first box to be processed to home box
+                pointer = bx
+              else:
+                # remaining boxes are nei boxes
+                pointer = box_cpu_nei_number[bx, k-1]
 
-              # loop for the number of particles in the current nei box
-              for j in range(NUMBER_PAR_PER_BOX):
-                r2 = rA_shared_v[wtx] + rB_shared_v[j] - DOT(rA_shared_x[wtx], rA_shared_y[wtx], rA_shared_z[wtx], rB_shared_x[j], rB_shared_y[j], rB_shared_z[j]) 
-                u2 = a2*r2
-                vij= math.exp(-u2)
-                fs = 2*vij
-                d_x = rA_shared_x[wtx] - rB_shared_x[j]
-                fxij=fs*d_x
-                d_y = rA_shared_y[wtx] - rB_shared_y[j]
-                fyij=fs*d_y
-                d_z = rA_shared_z[wtx] - rB_shared_z[j]
-                fzij=fs*d_z
-                fv_cpu_v[first_i+wtx] += qB_shared[j]*vij
-                fv_cpu_x[first_i+wtx] += qB_shared[j]*fxij
-                fv_cpu_y[first_i+wtx] += qB_shared[j]*fyij
-                fv_cpu_z[first_i+wtx] += qB_shared[j]*fzij
+              #  Setup parameters
 
-              # increment work thread index
-              wtx += NUMBER_THREADS
+              # nei box - box parameters
+              first_j = box_cpu_offset[pointer]
 
-            # reset work index
-            wtx = tx
+              # (enable the section below only if wanting to use shared memory)
+              # nei box - shared memory
+              while wtx < NUMBER_PAR_PER_BOX:
+                rB_shared_v[bx,wtx] = rv_cpu_v[first_j+wtx]
+                rB_shared_x[bx,wtx] = rv_cpu_x[first_j+wtx]
+                rB_shared_y[bx,wtx] = rv_cpu_y[first_j+wtx]
+                rB_shared_z[bx,wtx] = rv_cpu_z[first_j+wtx]
+                qB_shared[bx,wtx] = qv_cpu[first_j+wtx]
+                wtx += NUMBER_THREADS
+              wtx = tx
 
-            # synchronize after finishing force contributions from current nei box not to cause conflicts when starting next box
-            with openmp("barrier"):
-                pass
-    kend = omp_get_wtime()
+              # (enable the section below only if wanting to use shared memory)
+              # synchronize threads because in next section each thread accesses data brought in by different threads here
+              with openmp("barrier"):
+                  pass
+
+              #  Calculation
+
+              # loop for the number of particles in the home box
+              while wtx < NUMBER_PAR_PER_BOX:
+
+                # loop for the number of particles in the current nei box
+                for j in range(NUMBER_PAR_PER_BOX):
+                  r2 = rA_shared_v[bx,wtx] + rB_shared_v[bx,j] - DOT(rA_shared_x[bx,wtx], rA_shared_y[bx,wtx], rA_shared_z[bx,wtx], rB_shared_x[bx,j], rB_shared_y[bx,j], rB_shared_z[bx,j])
+                  u2 = a2*r2
+                  u2 = -1.0*u2
+                  vij= math.exp(u2)
+                  fs = 2*vij
+                  d_x = rA_shared_x[bx,wtx] - rB_shared_x[bx,j]
+                  fxij=fs*d_x
+                  d_y = rA_shared_y[bx,wtx] - rB_shared_y[bx,j]
+                  fyij=fs*d_y
+                  d_z = rA_shared_z[bx,wtx] - rB_shared_z[bx,j]
+                  fzij=fs*d_z
+                  fv_cpu_v[first_i+wtx] += qB_shared[bx,j]*vij
+                  fv_cpu_x[first_i+wtx] += qB_shared[bx,j]*fxij
+                  fv_cpu_y[first_i+wtx] += qB_shared[bx,j]*fyij
+                  fv_cpu_z[first_i+wtx] += qB_shared[bx,j]*fzij
+
+                # increment work thread index
+                wtx += NUMBER_THREADS
+
+              # reset work index
+              wtx = tx
+
+              # synchronize after finishing force contributions from current nei box not to cause conflicts when starting next box
+              with openmp("barrier"):
+                  pass
+
+      kend = omp_get_wtime()
+      print("Kernel execution time: ", float(kend-kstart), "s")
+
 
 def main():
   # assing default values
@@ -256,7 +279,7 @@ def main():
   NUMBER_PAR_PER_BOX = 100 # keep this low to allow more blocks that share shared memory to run concurrently, code does not work for larger than 110, more speedup can be achieved with larger number and no shared memory used
 
   NUMBER_THREADS = 128 # this should be roughly equal to NUMBER_PAR_PER_BOX for best performance
-  print("WG size of kernel = %d", NUMBER_THREADS)
+  print("WG size of kernel =", NUMBER_THREADS)
 
   # go through arguments
   if argc == 3:
@@ -338,19 +361,24 @@ def main():
   np.random.seed(2)
 
   # input (distances)
-  rv_cpu_v = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
-  rv_cpu_x = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
-  rv_cpu_y = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
-  rv_cpu_z = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  #rv_cpu_v = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  #rv_cpu_x = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  #rv_cpu_y = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  #rv_cpu_z = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  rv_cpu_v = np.full(dim_cpu_space_elem, 0.1, dtype=np.float32)
+  rv_cpu_x = np.full(dim_cpu_space_elem, 0.2, dtype=np.float32)
+  rv_cpu_y = np.full(dim_cpu_space_elem, 0.3, dtype=np.float32)
+  rv_cpu_z = np.full(dim_cpu_space_elem, 0.4, dtype=np.float32)
 
   # input (charge)
-  qv_cpu = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  #qv_cpu = np.random.uniform(0.1, 1.0, size=dim_cpu_space_elem)
+  qv_cpu = np.full(dim_cpu_space_elem, 0.5, dtype=np.float32)
 
   # output (forces)
-  fv_cpu_v = np.zeros(dim_cpu_space_elem)
-  fv_cpu_x = np.zeros(dim_cpu_space_elem)
-  fv_cpu_y = np.zeros(dim_cpu_space_elem)
-  fv_cpu_z = np.zeros(dim_cpu_space_elem)
+  fv_cpu_v = np.zeros(dim_cpu_space_elem, dtype=np.float32)
+  fv_cpu_x = np.zeros(dim_cpu_space_elem, dtype=np.float32)
+  fv_cpu_y = np.zeros(dim_cpu_space_elem, dtype=np.float32)
+  fv_cpu_z = np.zeros(dim_cpu_space_elem, dtype=np.float32)
 
   start = omp_get_wtime()
   core(dim_cpu_boxes1d_arg,
@@ -379,13 +407,12 @@ def main():
        NUMBER_THREADS,
        par_cpu_alpha)
   end = omp_get_wtime()
-  print("Device offloading time:", float(end-start) / 1000000)
-  print("Kernel execution time:", float(kend-kstart) / 1000000)
+  print("Device offloading time: ", float(end-start), "s")
 
   if DEBUG:
     offset = 395
     for g in range(10):
-      print("g=", g, fv_cpu_v[offset+g], fv_cpu_x[offset+g], fv_cpu_y[offset+g], fv_cpu_z[offset+g])
+      print(f"g= {g} {fv_cpu_v[offset+g]:.6f} {fv_cpu_x[offset+g]:.6f} {fv_cpu_y[offset+g]:.6f} {fv_cpu_z[offset+g]:.6f}")
 
   sys.exit(0)
 
