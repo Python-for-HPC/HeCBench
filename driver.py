61dd13287d356f243704d6b96931f0cab50a29fd
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2024-02-28 09:31:42 -0800

    Update driver and related for experimentation

--- driver.py
+++ driver.py
@@ -6,10 +6,11 @@ from pathlib import Path
 
 def compile(benchmark):
     cwd = Path.cwd() / f"src/{benchmark['name']}-omp"
+    print(f"\N{gear} => Compile {cwd}...")
     subprocess.run("make -f Makefile.lassen", shell=True, cwd=cwd, check=True)
 
 
-def run(rep, version, argkey, benchmark):
+def run(rep, version, argkey, benchmark, metrics):
     exe = ("python " if version == "pyomp" else "") + str(
         Path.cwd() / f"src/{benchmark['name']}-{version}/{benchmark['exe'][version]}"
     )
@@ -17,24 +18,27 @@ def run(rep, version, argkey, benchmark):
     cwd = Path.cwd() / f"results/pyomp/{benchmark['name']}"
     Path(cwd).mkdir(parents=True, exist_ok=True)
 
-    # Run to get execution times only for kernels, data transfer.
-    logfile = f"nvprof-{version}-{benchmark['name']}-{argkey}-{rep}.csv"
+    # Run through nvrpof, collect detailed metrics if enabled.
+    logfile = (
+        f"nvprof-metrics-{version}-{benchmark['name']}-{argkey}-{rep}.csv"
+        if metrics
+        else f"nvprof-{version}-{benchmark['name']}-{argkey}-{rep}.csv"
+    )
+
     cmd = (
         f"nvprof --print-gpu-trace --normalized-time-unit us --csv --log-file {logfile} "
+        + ("--metrics all " if metrics else "")
         + exe
         + " "
         + benchmark["inputs"][argkey]
     )
-    subprocess.run(cmd, shell=True, cwd=cwd, check=True)
 
-    # Run to get detailed profiler information.
-    logfile = f"nvprof-metrics-{version}-{benchmark['name']}-{argkey}-{rep}.csv"
-    cmd = (
-        f"nvprof --print-gpu-trace --metrics all --normalized-time-unit us --csv --log-file {logfile} "
-        + exe
-        + " "
-        + benchmark["inputs"][argkey]
-    )
+    print(f"\N{rocket} => Run cmd {cmd}")
+    check = cwd / logfile
+    if check.exists():
+        print(f"\u26A0\uFE0F WARNING: logfile {logfile} exists, will not re-run")
+        return
+
     subprocess.run(cmd, shell=True, cwd=cwd, check=True)
 
 
@@ -46,12 +50,35 @@ def main():
     parser.add_argument(
         "-r", "--repeats", help="experiment repeats", type=int, required=True
     )
+    parser.add_argument(
+        "-b", "--benchmarks", help="list of benchmarks to run", nargs="+"
+    )
+    parser.add_argument(
+        "-m",
+        "--metrics",
+        help="enable collecting detailed nvprof metrics",
+        action="store_true",
+    )
     args = parser.parse_args()
+
+    print("=== Run description ===")
+    print("Input", args.input)
+    print("Repeats", args.repeats)
+    print("Benchmarks", args.benchmarks)
+    print("Metrics", args.metrics)
+    print("=======================")
 
     with open(args.input, "r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    for benchmark in config["benchmarks"]:
+    benchmarks_to_run = (
+        [b for b in config["benchmarks"] if b["name"] in args.benchmarks]
+        if args.benchmarks
+        else config["benchmarks"]
+    )
+    assert len(benchmarks_to_run) >= 1, "Expected at least 1 benchmark to run"
+
+    for benchmark in benchmarks_to_run:
         assert len(benchmark["inputs"]) == 1, "Expected single input"
         assert (
             list(benchmark["inputs"])[0] == "default"
@@ -60,9 +87,11 @@ def main():
         for rep in range(args.repeats):
             # Run C openmp
             compile(benchmark)
-            run(rep, "omp", "default", benchmark)
+            run(rep, "omp", "default", benchmark, args.metrics)
             # Run PyOMP
-            run(rep, "pyomp", "default", benchmark)
+            run(rep, "pyomp", "default", benchmark, args.metrics)
+
+    print("\U0001f389 DONE!")
 
 
 if __name__ == "__main__":
--- postprocess.py
+++ postprocess.py
@@ -4,6 +4,58 @@ import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
+from IPython import embed
+
+
+def create_dataframe(bench_name, result_dir, version):
+    # Set all types to str because of awkwardness of nvprof CSV output.
+    dtypes_dict = {
+        "Start": str,
+        "Duration": str,
+        "Grid X": str,
+        "Grid Y": str,
+        "Grid Z": str,
+        "Block X": str,
+        "Block Y": str,
+        "Block Z": str,
+        "Registers Per Thread": str,
+        "Static SMem": str,
+        "Dynamic SMem": str,
+        "Size": str,
+        "Throughput": str,
+        "SrcMemType": str,
+        "DstMemType": str,
+        "Device": str,
+        "Context": str,
+        "Stream": str,
+        "Name": str,
+        "Correlation_ID": str,
+    }
+
+    files = result_dir.glob(f"nvprof-{version}-*.csv")
+    cat_df = pd.DataFrame()
+    for file in files:
+        rep = file.stem.split("-")[-1]
+        df = pd.read_csv(file, skiprows=3, dtype=dtypes_dict)
+        # Skip the first row after the header which contains units.
+        df = df[1:]
+        df = df[["Name", "Duration"]].astype({"Duration": float})
+        df["Version"] = version
+        df["Rep"] = int(rep) + 1
+        cat_df = pd.concat((cat_df, df))
+
+    # Normalize kernel names.
+    kernels = [x for x in cat_df.Name.unique() if "__omp_offload" in x]
+    kernels_norm = (
+        [bench_name]
+        if len(kernels) == 1
+        else [f"omp_kernel_{i+1}" for i in range(len(kernels))]
+    )
+    remap = dict(zip(kernels, kernels_norm))
+    remap["[CUDA memcpy DtoH]"] = "Memcpy DtoH"
+    remap["[CUDA memcpy HtoD]"] = "Memcpy HtoD"
+    cat_df.replace({"Name": remap}, inplace=True)
+    return cat_df
 
 
 def main():
@@ -11,46 +63,34 @@ def main():
         description="Postprocess measurement results for pyomp benchmarks."
     )
     parser.add_argument("-i", "--input", help="input configuration file", required=True)
+    parser.add_argument(
+        "-b", "--benchmarks", help="list of benchmarks to process", nargs="+"
+    )
     args = parser.parse_args()
 
     with open(args.input, "r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    database = pd.DataFrame()
+    benchmarks_to_process = (
+        [b for b in config["benchmarks"] if b["name"] in args.benchmarks]
+        if args.benchmarks
+        else config["benchmarks"]
+    )
+    assert len(benchmarks_to_process) >= 1, "Expected at least 1 benchmark to process"
 
-    def create_dataframes(result_dir, version):
-        files = result_dir.glob(f"nvprof-{version}-*.csv")
-        for file in files:
-            rep = file.stem.split("-")[-1]
-            df = pd.read_csv(file, skiprows=3)
-            # Skip the first row after the header which contains units of metrics.
-            df = df[1:]
-            df = df[["Name", "Duration"]].astype({"Duration": float})
-            df["Version"] = version
-            df["Rep"] = int(rep)
+    for benchmark in benchmarks_to_process:
+        cat_df = pd.DataFrame()
+        print(f"\N{rocket} => Processing {benchmark['name']}...")
+        result_dir = Path.cwd() / f"results/pyomp"
 
-        # Normalize kernel names.
-        kernels = [x for x in df.Name.unique() if "__omp_offload" in x]
-        kernels_norm = (
-            [benchmark["name"]]
-            if len(kernels) == 1
-            else [f"omp_kernel_{i+1}" for i in range(len(kernels))]
+        df = create_dataframe(benchmark["name"], result_dir / benchmark["name"], "omp")
+        cat_df = pd.concat((cat_df, df))
+        df = create_dataframe(
+            benchmark["name"], result_dir / benchmark["name"], "pyomp"
         )
-        remap = dict(zip(kernels, kernels_norm))
-        remap["[CUDA memcpy DtoH]"] = "Memcpy DtoH"
-        remap["[CUDA memcpy HtoD]"] = "Memcpy HtoD"
-        df.replace({"Name": remap}, inplace=True)
-        return df
+        cat_df = pd.concat((cat_df, df))
 
-    for benchmark in config["benchmarks"]:
-        result_dir = Path.cwd() / f"results/pyomp/{benchmark['name']}"
-
-        df = create_dataframes(result_dir, "omp")
-        database = pd.concat((database, df))
-        df = create_dataframes(result_dir, "pyomp")
-        database = pd.concat((database, df))
-
-        res = database.drop("Rep", axis=1).groupby(["Name", "Version"]).mean().unstack()
+        res = cat_df.drop("Rep", axis=1).groupby(["Name", "Version"]).mean().unstack()
         res.columns = res.columns.droplevel(0)
         ax = res.plot.bar(width=0.6)
         ax.set_yscale("log", base=2)
@@ -63,8 +103,9 @@ def main():
         for container in ax.containers:
             ax.bar_label(container, fmt="%.2f")
         plt.tight_layout()
-        outdir = Path.cwd() / f"results/pyomp"
-        plt.savefig(outdir / f"time-{benchmark['name']}.pdf")
+        plt.savefig(result_dir / f"time-{benchmark['name']}.pdf")
+
+    print("\U0001f389 DONE!")
 
 
 if __name__ == "__main__":
