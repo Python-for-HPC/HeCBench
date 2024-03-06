e3bad53eb0636159123de00ec0bd469e5ed1b73f
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2024-03-06 14:30:43 -0800

    Fix fluidsim

--- src/fluidSim-pyomp/kernels.py
+++ src/fluidSim-pyomp/kernels.py
@@ -1,64 +1,53 @@
 from numba import njit
 from numba.openmp import openmp_context as openmp
-from numba.openmp import omp_get_wtime, omp_set_num_threads, omp_get_num_threads, omp_get_num_devices, omp_is_initial_device, omp_get_team_num, omp_get_thread_num
+from numba.openmp import (
+    omp_get_wtime,
+    omp_set_num_threads,
+    omp_get_num_threads,
+    omp_get_num_devices,
+    omp_is_initial_device,
+    omp_get_team_num,
+    omp_get_thread_num,
+)
 import numpy as np
+
 # Thread block size
 
-@njit(inline='always')
+
+@njit(inline="always")
 def dot(a, b):
     return (a[0] * b[0] + a[1] * b[1]) + (a[2] * b[2] + a[3] * b[3])
 
+
 # Calculates equivalent distribution
 
-@njit(inline='always')
-def ced(rho, weight, diro, u):
-    u2 = (u[0] * u[0]) + (u[1] * u[1])
-    eu = (diro[0] * u[0]) + (diro[1] * u[1])
+
+@njit(inline="always")
+def ced(rho, weight, dirx, diry, u_0, u_1):
+    u2 = (u_0 * u_0) + (u_1 * u_1)
+    eu = (dirx * u_0) + (diry * u_1)
     return rho * weight * (1.0 + 3.0 * eu + 4.5 * eu * eu - 1.5 * u2)
 
-@njit(inline='always')
-def newPos(p, diro):
-    return p + np.round(diro).astype(int)
-    """
-    np = [0] * 8
-    np[0] = p + int(diro[0])
-    np[1] = p + int(diro[1])
-    np[2] = p + int(diro[2])
-    np[3] = p + int(diro[3])
-    np[4] = p + int(diro[4])
-    np[5] = p + int(diro[5])
-    np[6] = p + int(diro[6])
-    np[7] = p + int(diro[7])
-    return np
-    """
-
-@njit(inline='always')
-def fma8(a, b, c):
-    return (a * b) + c
-    """
-    r = [0] * 8
-    r[0] = a * b[0] + c[0]
-    r[1] = a * b[1] + c[1]
-    r[2] = a * b[2] + c[2]
-    r[3] = a * b[3] + c[3]
-    r[4] = a * b[4] + c[4]
-    r[5] = a * b[5] + c[5]
-    r[6] = a * b[6] + c[6]
-    r[7] = a * b[7] + c[7]
-    return r
-    """
 
 @njit
-def lbm(width, height, if0, of0, if1234, of1234, if5678, of5678, typeo, dirX, dirY, weight, omega):
-    e1234 = np.zeros(4, dtype=np.int32)
-    e5678 = np.zeros(4, dtype=np.int32)
-    u = np.zeros(2, dtype=np.int32) # Velocity
-    x1234 = np.zeros(4, dtype=np.float64)
-    y1234 = np.zeros(4, dtype=np.float64)
-    x5678 = np.zeros(4, dtype=np.float64)
-    y5678 = np.zeros(4, dtype=np.float64)
-    temp  = np.zeros(4, dtype=np.float64)
-    with openmp("""target teams distribute parallel for collapse(2) thread_limit(256) map(to: e1234, e5678, u, x1234, y1234, x5678, y5678, temp)"""):
+def lbm(
+    width,
+    height,
+    if0,
+    of0,
+    if1234,
+    of1234,
+    if5678,
+    of5678,
+    typeo,
+    dirX,
+    dirY,
+    weight,
+    omega,
+):
+    with openmp(
+        """target teams distribute parallel for collapse(2) thread_limit(256) device(1)"""
+    ):
         for idy in range(height):
             for idx in range(width):
                 pos = idx + width * idy
@@ -68,97 +57,159 @@ def lbm(width, height, if0, of0, if1234, of1234, if5678, of5678, typeo, dirX, di
                 f5678 = if5678[pos]
                 # intermediate results
                 e0 = 0
-                rho = 0 # Density
-                """
-                e1234 = np.zeros(4, dtype=np.int32)
-                e5678 = np.zeros(4, dtype=np.int32)
-                u = np.zeros(2, dtype=np.int32) # Velocity
-                """
+                rho = 0  # Density
                 # Collide
-                if typeo[pos]: # Boundary
+                if typeo[pos]:  # Boundary
                     # Swap directions
-                    e1234[0] = f1234[2]
-                    e1234[1] = f1234[3]
-                    e1234[2] = f1234[0]
-                    e1234[3] = f1234[1]
-                    e5678[0] = f5678[2]
-                    e5678[1] = f5678[3]
-                    e5678[2] = f5678[0]
-                    e5678[3] = f5678[1]
+                    e1234_0 = f1234[2]
+                    e1234_1 = f1234[3]
+                    e1234_2 = f1234[0]
+                    e1234_3 = f1234[1]
+                    e5678_0 = f5678[2]
+                    e5678_1 = f5678[3]
+                    e5678_2 = f5678[0]
+                    e5678_3 = f5678[1]
                     rho = 0
-                    u[:] = 0
-                else: # Fluid
+                    u_0 = 0
+                    u_1 = 0
+                else:  # Fluid
                     # Compute rho and u
                     # Rho is computed by doing a reduction on f
-                    temp[:] = f1234 + f5678
-                    rho = f0 + temp[0] + temp[1] + temp[2] + temp[3]
+                    elem_sum_0 = f1234[0] + f5678[0]
+                    elem_sum_1 = f1234[1] + f5678[1]
+                    elem_sum_2 = f1234[2] + f5678[2]
+                    elem_sum_3 = f1234[3] + f5678[3]
+                    rho = f0 + elem_sum_0 + elem_sum_1 + elem_sum_2 + elem_sum_3
                     # Compute velocity in x and y directions
-                    x1234[:] = dirX[0:4]
-                    y1234[:] = dirY[0:4]
-                    x5678[:] = dirX[4:8]
-                    y5678[:] = dirY[4:8]
-                    """
-                    u[0] = (dot(f1234, x1234) + dot(f5678, x5678)) / rho
-                    u[1] = (dot(f1234, y1234) + dot(f5678, y5678)) / rho
+                    # x1234[:] = dirX[0:4]
+                    # y1234[:] = dirY[0:4]
+                    # x5678[:] = dirX[4:8]
+                    # y5678[:] = dirY[4:8]
+                    u_0 = (dot(f1234, dirX[0:4]) + dot(f5678, dirX[4:8])) / rho
+                    u_1 = (dot(f1234, dirY[0:4]) + dot(f5678, dirY[4:8])) / rho
+
                     # Compute f
-                    e0 = ced(rho, weight[0], [0, 0], u)
-                    e1234[0] = ced(rho, weight[1], [dirX[0], dirY[0]], u)
-                    e1234[1] = ced(rho, weight[2], [dirX[1], dirY[1]], u)
-                    e1234[2] = ced(rho, weight[3], [dirX[2], dirY[2]], u)
-                    e1234[3] = ced(rho, weight[4], [dirX[3], dirY[3]], u)
-                    e5678[0] = ced(rho, weight[5], [dirX[4], dirY[4]], u)
-                    e5678[1] = ced(rho, weight[6], [dirX[5], dirY[5]], u)
-                    e5678[2] = ced(rho, weight[7], [dirX[6], dirY[6]], u)
-                    e5678[3] = ced(rho, weight[8], [dirX[7], dirY[7]], u)
+                    e0 = ced(rho, weight[0], 0, 0, u_0, u_1)
+                    e1234_0 = ced(rho, weight[1], dirX[0], dirY[0], u_0, u_1)
+                    e1234_1 = ced(rho, weight[2], dirX[1], dirY[1], u_0, u_1)
+                    e1234_2 = ced(rho, weight[3], dirX[2], dirY[2], u_0, u_1)
+                    e1234_3 = ced(rho, weight[4], dirX[3], dirY[3], u_0, u_1)
+                    e5678_0 = ced(rho, weight[5], dirX[4], dirY[4], u_0, u_1)
+                    e5678_1 = ced(rho, weight[6], dirX[5], dirY[5], u_0, u_1)
+                    e5678_2 = ced(rho, weight[7], dirX[6], dirY[6], u_0, u_1)
+                    e5678_3 = ced(rho, weight[8], dirX[7], dirY[7], u_0, u_1)
                     e0 = (1.0 - omega) * f0 + omega * e0
-                    e1234 = [(1.0 - omega) * f1234[i] + omega * e1234[i] for i in range(4)]
-                    e5678 = [(1.0 - omega) * f5678[i] + omega * e5678[i] for i in range(4)]
-                    """
+                    # e1234 = [
+                    #    (1.0 - omega) * f1234[i] + omega * e1234[i] for i in range(4)
+                    # ]
+                    e1234_0 = (1.0 - omega) * f1234[0] + omega * e1234_0
+                    e1234_1 = (1.0 - omega) * f1234[1] + omega * e1234_1
+                    e1234_2 = (1.0 - omega) * f1234[2] + omega * e1234_2
+                    e1234_3 = (1.0 - omega) * f1234[3] + omega * e1234_3
+
+                    # e5678 = [
+                    #    (1.0 - omega) * f5678[i] + omega * e5678[i] for i in range(4)
+                    # ]
+                    e5678_0 = (1.0 - omega) * f5678[0] + omega * e5678_0
+                    e5678_1 = (1.0 - omega) * f5678[1] + omega * e5678_1
+                    e5678_2 = (1.0 - omega) * f5678[2] + omega * e5678_2
+                    e5678_3 = (1.0 - omega) * f5678[3] + omega * e5678_3
                 # Propagate
-                t3 = idx > 0          # Not on Left boundary
+                t3 = idx > 0  # Not on Left boundary
                 t1 = idx < width - 1  # Not on Right boundary
-                t4 = idy > 0          # Not on Upper boundary
-                t2 = idy < height - 1 # Not on lower boundary
+                t4 = idy > 0  # Not on Upper boundary
+                t2 = idy < height - 1  # Not on lower boundary
                 if t1 and t2 and t3 and t4:
                     # New positions to write (Each thread will write 8 values)
                     # Note the propagation sources (e.g. f1234) imply the OLD locations for each thread
-                    pass
-                    """
-                    nX = newPos(idx, dirX)
-                    nY = newPos(idy, dirY)
-                    nPos = fma8(width, nY, nX)
+                    # nX = newPos(idx, dirX)
+                    # nY = newPos(idy, dirY)
+                    # nPos = fma8(width, nY, nX)
                     # Write center distribution to thread's location
                     of0[pos] = e0
                     # Propagate to right cell
-                    of1234[nPos[0]] = e1234[0]
+                    nX = idx + dirX[0]
+                    nY = idy + dirY[0]
+                    nPos = width * nY + nX
+                    of1234[nPos, 0] = e1234_0
                     # Propagate to Lower cell
-                    of1234[nPos[1]] = e1234[1]
-                    # Propagate to left cell
-                    of1234[nPos[2]] = e1234[2]
+                    nX = idx + dirX[1]
+                    nY = idy + dirY[1]
+                    nPos = width * nY + nX
+                    of1234[nPos, 1] = e1234_1
+                    ## Propagate to left cell
+                    nX = idx + dirX[2]
+                    nY = idy + dirY[2]
+                    nPos = width * nY + nX
+                    of1234[nPos, 2] = e1234_2
                     # Propagate to Upper cell
-                    of1234[nPos[3]] = e1234[3]
+                    nX = idx + dirX[3]
+                    nY = idy + dirY[3]
+                    nPos = width * nY + nX
+                    of1234[nPos, 3] = e1234_3
                     # Propagate to Lower-Right cell
-                    of5678[nPos[4]] = e5678[0]
+                    nX = idx + dirX[4]
+                    nY = idy + dirY[4]
+                    nPos = width * nY + nX
+                    of5678[nPos, 0] = e5678_0
                     # Propogate to Lower-Left cell
-                    of5678[nPos[5]] = e5678[1]
+                    nX = idx + dirX[5]
+                    nY = idy + dirY[5]
+                    nPos = width * nY + nX
+                    of5678[nPos, 1] = e5678_1
                     # Propagate to Upper-Left cell
-                    of5678[nPos[6]] = e5678[2]
+                    nX = idx + dirX[6]
+                    nY = idy + dirY[6]
+                    nPos = width * nY + nX
+                    of5678[nPos, 2] = e5678_2
                     # Propagate to Upper-Right cell
-                    of5678[nPos[7]] = e5678[3]
-                    """
+                    nX = idx + dirX[7]
+                    nY = idy + dirY[7]
+                    nPos = width * nY + nX
+                    of5678[nPos, 3] = e5678_3
+
 
 @njit
-def fluidSim(iterations, omega, dims, h_type, u, rho, dirX, dirY, h_weight, h_if0, h_if1234, h_if5678, h_of0, h_of1234, h_of5678):
+def fluidSim(
+    iterations,
+    omega,
+    dims,
+    h_type,
+    dirX,
+    dirY,
+    h_weight,
+    h_if0,
+    h_if1234,
+    h_if5678,
+    h_of0,
+    h_of1234,
+    h_of5678,
+):
     width = dims[0]
     height = dims[1]
-    temp = width * height
-    dbl_size = temp
     h_of0[:] = h_if0
     h_of1234[:] = h_if1234
     h_of5678[:] = h_if5678
-    with openmp("""target data map (to: h_of, h_of1234, h_of5678, h_weight, h_type) map (tofrom: h_if0, h_if1234, h_if5678)"""):
+    with openmp(
+        """target data map(to: h_of0, h_of1234, h_of5678, h_weight, h_type) map(tofrom: h_if0, h_if1234, h_if5678) device(1)"""
+    ):
+        t1 = omp_get_wtime()
         for _ in range(iterations):
-            lbm(width, height, h_if0, h_of0, h_if1234, h_of1234, h_if5678, h_of5678, h_type, dirX, dirY, h_weight, omega)
+            lbm(
+                width,
+                height,
+                h_if0,
+                h_of0,
+                h_if1234,
+                h_of1234,
+                h_if5678,
+                h_of5678,
+                h_type,
+                dirX,
+                dirY,
+                h_weight,
+                omega,
+            )
             # Swap device buffers
             temp0 = h_of0
             temp1234 = h_of1234
@@ -169,6 +220,9 @@ def fluidSim(iterations, omega, dims, h_type, u, rho, dirX, dirY, h_weight, h_if
             h_if0 = temp0
             h_if1234 = temp1234
             h_if5678 = temp5678
+    t2 = omp_get_wtime()
+    print("Average kernel execution time", (t2 - t1) / iterations, "(s)\n")
+
     if iterations % 2 == 0:
         h_of0[:] = h_if0
         h_of1234[:] = h_if1234
--- src/fluidSim-pyomp/main.py
+++ src/fluidSim-pyomp/main.py
@@ -1,17 +1,38 @@
 import sys
 import random
 import numpy as np
+from numba import njit
 from kernels import fluidSim
+import time
 
+
+@njit
 def computefEq(rho, weight, diro, velocity):
     u2 = velocity[0] ** 2 + velocity[1] ** 2
     eu = diro[0] * velocity[0] + diro[1] * velocity[1]
-    return rho * weight * (1.0 + 3.0 * eu + 4.5 * eu ** 2 - 1.5 * u2)
+    return rho * weight * (1.0 + 3.0 * eu + 4.5 * eu**2 - 1.5 * u2)
+
 
 # error bound
 EPISON = 1e-3
 
-def reference(iterations, omega, dims, h_type, rho, diro, weight, h_if0, h_if1234, h_if5678, h_of0, h_of1234, h_of5678):
+
+@njit
+def reference(
+    iterations,
+    omega,
+    dims,
+    h_type,
+    rho,
+    diro,
+    weight,
+    h_if0,
+    h_if1234,
+    h_if5678,
+    h_of0,
+    h_of1234,
+    h_of5678,
+):
     v_of0 = h_of0
     v_of1234 = h_of1234
     v_of5678 = h_of5678
@@ -30,17 +51,11 @@ def reference(iterations, omega, dims, h_type, rho, diro, weight, h_if0, h_if123
     v_of0[:] = h_if0
     v_of1234[:] = h_if1234
     v_of5678[:] = h_if5678
-    #v_if0[:] = np.copy(h_if0)
-    #v_if1234[:] = np.copy(h_if1234)
-    #v_if5678[:] = np.copy(h_if5678)
-    #v_of0[:] = np.copy(h_if0)
-    #v_of1234[:] = np.copy(h_if1234)
-    #v_of5678[:] = np.copy(h_if5678)
     for i in range(iterations):
         for y in range(dims[1]):
             for x in range(dims[0]):
                 pos = x + y * dims[0]
-                if h_type[pos] == 1: # Boundary
+                if h_type[pos] == 1:  # Boundary
                     v_ef0[pos] = v_if0[pos]
                     temp1 = v_if1234[pos, 2]
                     temp2 = v_if1234[pos, 3]
@@ -59,11 +74,39 @@ def reference(iterations, omega, dims, h_type, rho, diro, weight, h_if0, h_if123
                     v_ef5678[pos, 2] = temp3
                     v_ef5678[pos, 3] = temp4
                     rho[pos] = 0
-                else: # Fluid
+                else:  # Fluid
                     vel = np.zeros(2)
-                    den = v_if0[pos] + v_if1234[pos, 0] + v_if1234[pos, 1] + v_if1234[pos, 2] + v_if1234[pos, 3] + v_if5678[pos, 0] + v_if5678[pos, 1] + v_if5678[pos, 2] + v_if5678[pos, 3]
-                    vel[0] = v_if1234[pos, 0] * diro[1][0] + v_if1234[pos, 1] * diro[2][0] + v_if1234[pos, 2] * diro[3][0] + v_if1234[pos, 3] * diro[4][0] + v_if5678[pos, 0] * diro[5][0] + v_if5678[pos, 1] * diro[6][0] + v_if5678[pos, 2] * diro[7][0] + v_if5678[pos, 3] * diro[8][0]
-                    vel[1] = v_if1234[pos, 0] * diro[1][1] + v_if1234[pos, 1] * diro[2][1] + v_if1234[pos, 2] * diro[3][1] + v_if1234[pos, 3] * diro[4][1] + v_if5678[pos, 0] * diro[5][1] + v_if5678[pos, 1] * diro[6][1] + v_if5678[pos, 2] * diro[7][1] + v_if5678[pos, 3] * diro[8][1]
+                    den = (
+                        v_if0[pos]
+                        + v_if1234[pos, 0]
+                        + v_if1234[pos, 1]
+                        + v_if1234[pos, 2]
+                        + v_if1234[pos, 3]
+                        + v_if5678[pos, 0]
+                        + v_if5678[pos, 1]
+                        + v_if5678[pos, 2]
+                        + v_if5678[pos, 3]
+                    )
+                    vel[0] = (
+                        v_if1234[pos, 0] * diro[1][0]
+                        + v_if1234[pos, 1] * diro[2][0]
+                        + v_if1234[pos, 2] * diro[3][0]
+                        + v_if1234[pos, 3] * diro[4][0]
+                        + v_if5678[pos, 0] * diro[5][0]
+                        + v_if5678[pos, 1] * diro[6][0]
+                        + v_if5678[pos, 2] * diro[7][0]
+                        + v_if5678[pos, 3] * diro[8][0]
+                    )
+                    vel[1] = (
+                        v_if1234[pos, 0] * diro[1][1]
+                        + v_if1234[pos, 1] * diro[2][1]
+                        + v_if1234[pos, 2] * diro[3][1]
+                        + v_if1234[pos, 3] * diro[4][1]
+                        + v_if5678[pos, 0] * diro[5][1]
+                        + v_if5678[pos, 1] * diro[6][1]
+                        + v_if5678[pos, 2] * diro[7][1]
+                        + v_if5678[pos, 3] * diro[8][1]
+                    )
                     vel[0] /= den
                     vel[1] /= den
                     v_ef0[pos] = computefEq(den, weight[0], diro[0], vel)
@@ -76,16 +119,32 @@ def reference(iterations, omega, dims, h_type, rho, diro, weight, h_if0, h_if123
                     v_ef5678[pos, 2] = computefEq(den, weight[7], diro[7], vel)
                     v_ef5678[pos, 3] = computefEq(den, weight[8], diro[8], vel)
                     v_ef0[pos] = (1 - omega) * v_if0[pos] + omega * v_ef0[pos]
-                    v_ef1234[pos, 0] = (1 - omega) * v_if1234[pos, 0] + omega * v_ef1234[pos, 0]
-                    v_ef1234[pos, 1] = (1 - omega) * v_if1234[pos, 1] + omega * v_ef1234[pos, 1]
-                    v_ef1234[pos, 2] = (1 - omega) * v_if1234[pos, 2] + omega * v_ef1234[pos, 2]
-                    v_ef1234[pos, 3] = (1 - omega) * v_if1234[pos, 3] + omega * v_ef1234[pos, 3]
-                    v_ef5678[pos, 0] = (1 - omega) * v_if5678[pos, 0] + omega * v_ef5678[pos, 0]
-                    v_ef5678[pos, 1] = (1 - omega) * v_if5678[pos, 1] + omega * v_ef5678[pos, 1]
-                    v_ef5678[pos, 2] = (1 - omega) * v_if5678[pos, 2] + omega * v_ef5678[pos, 2]
-                    v_ef5678[pos, 3] = (1 - omega) * v_if5678[pos, 3] + omega * v_ef5678[pos, 3]
-        for y in range(1, dims[1]-1):
-            for x in range(1, dims[0]-1):
+                    v_ef1234[pos, 0] = (1 - omega) * v_if1234[
+                        pos, 0
+                    ] + omega * v_ef1234[pos, 0]
+                    v_ef1234[pos, 1] = (1 - omega) * v_if1234[
+                        pos, 1
+                    ] + omega * v_ef1234[pos, 1]
+                    v_ef1234[pos, 2] = (1 - omega) * v_if1234[
+                        pos, 2
+                    ] + omega * v_ef1234[pos, 2]
+                    v_ef1234[pos, 3] = (1 - omega) * v_if1234[
+                        pos, 3
+                    ] + omega * v_ef1234[pos, 3]
+                    v_ef5678[pos, 0] = (1 - omega) * v_if5678[
+                        pos, 0
+                    ] + omega * v_ef5678[pos, 0]
+                    v_ef5678[pos, 1] = (1 - omega) * v_if5678[
+                        pos, 1
+                    ] + omega * v_ef5678[pos, 1]
+                    v_ef5678[pos, 2] = (1 - omega) * v_if5678[
+                        pos, 2
+                    ] + omega * v_ef5678[pos, 2]
+                    v_ef5678[pos, 3] = (1 - omega) * v_if5678[
+                        pos, 3
+                    ] + omega * v_ef5678[pos, 3]
+        for y in range(1, dims[1] - 1):
+            for x in range(1, dims[0] - 1):
                 src_pos = x + dims[0] * y
                 for k in range(9):
                     nx = x + int(diro[k][0])
@@ -112,9 +171,6 @@ def reference(iterations, omega, dims, h_type, rho, diro, weight, h_if0, h_if123
         v_if0, v_of0 = v_of0, v_if0
         v_if1234, v_of1234 = v_of1234, v_if1234
         v_if5678, v_of5678 = v_of5678, v_if5678
-    #h_of0[:] = np.copy(v_if0)
-    #h_of1234[:] = np.copy(v_if1234)
-    #h_of5678[:] = np.copy(v_if5678)
     h_of0[:] = v_if0
     h_of1234[:] = v_if1234
     h_of5678[:] = v_if5678
@@ -124,11 +180,39 @@ def reference(iterations, omega, dims, h_type, rho, diro, weight, h_if0, h_if123
         v_if5678, v_of5678 = v_of5678, v_if5678
     return h_of0, h_of1234, h_of5678
 
+
 def verify(h_of0, h_of1234, h_of5678, v_of0, v_of1234, v_of5678):
-    ok = (np.allclose(h_of0, v_of0, atol=EPISON) and 
-          np.allclose(h_of1234, v_of1234, atol=EPISON) and
-          np.allclose(h_of5678, v_of5678, atol=EPISON))
+    ok = (
+        np.allclose(h_of0, v_of0, atol=EPISON)
+        and np.allclose(h_of1234, v_of1234, atol=EPISON)
+        and np.allclose(h_of5678, v_of5678, atol=EPISON)
+    )
     print("PASS" if ok else "FAIL")
+
+
+@njit
+def init(dims, u, w, e, h_if0, h_if1234, h_if5678, h_type):
+    # Initial velocity is nonzero for verifying host and device results
+    u0 = [0.01, 0.01]
+    random.seed(123)
+    for y in range(dims[1]):
+        for x in range(dims[0]):
+            pos = x + y * dims[0]
+            # Random values for verification
+            den = random.randint(1, 10)
+            # Initialize the velocity buffer
+            u[pos] = u0
+            # Initialize the frequency (i.e. the number of particles in
+            # a cell going in each velocity direction)
+            h_if0[pos] = computefEq(den, w[0], e[0], u0)
+            h_if1234[pos] = [computefEq(den, w[i], e[i], u0) for i in range(1, 5)]
+            h_if5678[pos] = [computefEq(den, w[i], e[i], u0) for i in range(5, 9)]
+            # Initialize boundary cells
+            if x == 0 or x == (dims[0] - 1) or y == 0 or y == (dims[1] - 1):
+                h_type[pos] = True
+            # Initialize fluid cells
+            else:
+                h_type[pos] = False
 
 
 def main():
@@ -137,17 +221,29 @@ def main():
         return 1
 
     iterations = int(sys.argv[1])  # Simulation iterations
-    #lbm_width = 5                 # Dimension of LBM simulation area
-    #lbm_height = 5
-    lbm_width = 256                 # Dimension of LBM simulation area
+    lbm_width = 256  # Dimension of LBM simulation area
     lbm_height = 256
     dims = (lbm_width, lbm_height)
     temp = dims[0] * dims[1]
 
     # Nine velocity directions for each cell
-    e = np.array([[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [-1, -1], [1, -1]])
+    e = np.array(
+        [[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [-1, -1], [1, -1]]
+    )
     # Weights depend on lattice geometry
-    w = np.array([4.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/36.0, 1.0/36.0, 1.0/36.0, 1.0/36.0])
+    w = np.array(
+        [
+            4.0 / 9.0,
+            1.0 / 9.0,
+            1.0 / 9.0,
+            1.0 / 9.0,
+            1.0 / 9.0,
+            1.0 / 36.0,
+            1.0 / 36.0,
+            1.0 / 36.0,
+            1.0 / 36.0,
+        ]
+    )
     # Omega equals time step divided by Tau (viscosity of the fluid)
     omega = 1.2
 
@@ -176,44 +272,65 @@ def main():
     u = np.zeros((temp, 2))
 
     # Initial velocity is nonzero for verifying host and device results
-    u0 = [0.01, 0.01]
-    random.seed(123)
-    for y in range(dims[1]):
-        for x in range(dims[0]):
-            pos = x + y * dims[0]
-            # Random values for verification
-            den = random.randint(1, 10)
-            # Initialize the velocity buffer
-            u[pos] = u0
-            # Initialize the frequency (i.e. the number of particles in
-            # a cell going in each velocity direction)
-            h_if0[pos] = computefEq(den, w[0], e[0], u0)
-            h_if1234[pos] = [computefEq(den, w[i], e[i], u0) for i in range(1, 5)]
-            h_if5678[pos] = [computefEq(den, w[i], e[i], u0) for i in range(5, 9)]
-            # Initialize boundary cells
-            if x == 0 or x == (dims[0] - 1) or y == 0 or y == (dims[1] - 1):
-                h_type[pos] = True
-            # Initialize fluid cells
-            else:
-                h_type[pos] = False
+    init(dims, u, w, e, h_if0, h_if1234, h_if5678, h_type)
 
     # Initialize direction vectors for each cell
-    dirX = e[1:, 0]
-    dirY = e[1:, 1]
+    dirX = np.empty(e.shape[0] - 1, dtype=np.int64)
+    dirX[:] = e[1:, 0]
+    dirY = np.empty(e.shape[0] - 1, dtype=np.int64)
+    dirY[:] = e[1:, 1]
 
-    reference(iterations, omega, dims, h_type, rho, e, w,
-              h_if0, h_if1234, h_if5678,
-              v_of0, v_of1234, v_of5678)
+    t1 = time.perf_counter()
+    reference(
+        iterations,
+        omega,
+        dims,
+        h_type,
+        rho,
+        e,
+        w,
+        h_if0,
+        h_if1234,
+        h_if5678,
+        v_of0,
+        v_of1234,
+        v_of5678,
+    )
+    t2 = time.perf_counter()
+    print("Reference execution time", t2 - t1, "s")
 
-    fluidSim(iterations, omega, dims, h_type, u, rho, dirX, dirY, w,
-             h_if0, h_if1234, h_if5678,
-             h_of0, h_of1234, h_of5678)
+    # TODO: Fix eager compilation.
+    # t1 = time.perf_counter()
+    # fluidSim.compile(
+    #    """void(
+    #        int64, float64, UniTuple(int64, 2), boolean[:], int64[:], int64[:], float64[:],
+    #        float64[:], float64[:, :], float64[:, :], float64[:], float64[:, :], float64[:, :])"""
+    # )
+    # t2 = time.perf_counter()
+    # print("Compilation time", t2 - t1, "s")
 
-    verify(h_of0, h_of1234, h_of5678,
-           v_of0, v_of1234, v_of5678)
+    t1 = time.perf_counter()
+    fluidSim(
+        iterations,
+        omega,
+        dims,
+        h_type,
+        dirX,
+        dirY,
+        w,
+        h_if0,
+        h_if1234,
+        h_if5678,
+        h_of0,
+        h_of1234,
+        h_of5678,
+    )
+
+    t2 = time.perf_counter()
+    print("Total GPU execution time", t2 - t1, "s")
+
+    verify(h_of0, h_of1234, h_of5678, v_of0, v_of1234, v_of5678)
+
 
 if __name__ == "__main__":
     main()
-
-
-
